6d85598178a39b67326feb50dc46d3e766cb1d2f
Satuyan <ragnolur@gmail.com>
2016-05-28 02:48:10 +0300

    Data for index

--- application/controllers/Main.php
+++ application/controllers/Main.php
@@ -8,18 +8,20 @@ class Main extends CI_Controller {
 		$this->load->model('categories_model');
 		$this->load->model('filters_model');
 		$this->load->model('service_model');
-//		$data = array();
 		$this->data['categories'] = $this->categories_model->getCategories();
 		$this->data['filters'] = $this->filters_model->getFilters();
+
 		$services = $this->service_model->getServices();
 		for($i=0;$i<count($services);$i++){
 			$services[$i]['coordinates'] = json_decode($services[$i]['coordinates']);
 		};
 		$this->data['services'] = $services;
+
 		foreach ($this->data['categories'] as $key=>$value) {
 			$params['category_ids'] = $value['id'];
 			$this->data['categories'][$key][] = $this->filters_model->getFilters($params);
 		}
+
 		$this->load->view('map/home', $this->data);
 	}
 }
