f78e0d0739d0f72682d0ea249d5fc42dd8f8237e
ArgeSwarge <argecata@mail.ru>
2016-06-06 00:59:17 +0300

    Два дошика

--- application/views/admin/header.php
+++ application/views/admin/header.php
@@ -12,7 +12,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     <link rel="stylesheet" href="/front-end/css/main.css">
     <link rel="stylesheet" href="/front-end/css/bootstrap.css">
     <link rel="stylesheet" href="/front-end/css/bootstrap-theme.css">
-
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,300italic,400italic,600,600italic,700,700italic,800,800italic' rel='stylesheet' type='text/css'>
 
 </head>
 <body>
