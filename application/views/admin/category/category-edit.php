4c0b88b5d79a656f49b1b257dce56b90048b9c83
Nolfneo <igorok901@gmail.com>
2016-06-06 20:43:03 +0300

    сука, хватит удалять мой код!

--- application/views/admin/category/category-edit.php
+++ application/views/admin/category/category-edit.php
@@ -1,9 +1,9 @@
 <div class="col-lg-10">
     <div class="admin-content">
-        <form role="form" action="/admin/category/add" method="post" style="max-width: 400px;">
+        <form action="/admin/category/edit/<?php echo $id?>" method="post" style="max-width: 400px;">
             <div class="form-group">
                 <label for="name">Назва</label>
-                <input name="name" type="text" class="form-control" placeholder="Назва категорії">
+                <input value="<?php echo $name?>" name="name" type="text" class="form-control" placeholder="Назва категорії">
             </div>
             <button type="submit" class="btn btn-default addcontragent">Зберегти</button>
         </form>
